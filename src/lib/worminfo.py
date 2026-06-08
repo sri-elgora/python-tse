7bf5e0acb7bdbcbfe8c48e495e69558afca1447e
Steve Richmann <sri@elgora.de>
2026-06-08 15:19:21 +0200

    Update 6.0.0

--- src/lib/worminfo.py
+++ src/lib/worminfo.py
@@ -1,20 +1,3 @@
-# TSE Connection Library
-# Copyright (C) 2020 Bernd Wurst <bernd@mosterei-wurst.de>
-# 
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 3 of the License, or (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-# 
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program; if not, write to the Free Software Foundation,
-# Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-
 import datetime
 from ctypes import *
 from wormtypes import *
@@ -29,14 +12,11 @@ class Worm_Info:
         self.wormlib.worm_info_new.restype = WormInfo
         self.info = cast(self.wormlib.worm_info_new(self.ctx), WormInfo)
         self.wormlib.worm_info_read.restype = WormError
-        self.wormlib.worm_info_read.argtypes = (WormInfo, )
+        self.wormlib.worm_info_read.argtypes = (WormInfo,)
         self.update()
-        
-    def update(self):
-        ret = self.wormlib.worm_info_read(self.info)
-        #WormError_to_exception(ret)
-        #Wenn wir hier eine Exception ausgeben, bricht das an manchen Stellen, falls die TSE nicht angesteckt ist...
 
+    def update(self):
+        self.wormlib.worm_info_read(self.info)
 
     def __del__(self):
         if self.info:
@@ -44,86 +24,131 @@ class Worm_Info:
             self.info = None
 
     def __getattr__(self, key):
-        if key in ['isDevelopmentFirmware', 'isStoreOpen', 'hasValidTime', 'hasPassedSelfTest', 
-                   'isCtssInterfaceActive', 'isErsInterfaceActive', 'isExportEnabledIfCspTestFails',
-                   'isDataImportInProgress', 'isTransactionInProgress', 'hasChangedPuk', 'hasChangedAdminPin',
-                   'hasChangedTimeAdminPin']:
+        # bool-Attribute (uint32, als bool zurückgegeben)
+        if key in [
+            'isDevelopmentFirmware', 'isTSEv2',
+            'hasValidTime', 'hasPassedSelfTest',
+            'isCtssInterfaceActive', 'isErsInterfaceActive',
+            'isExportEnabledIfCspTestFails',
+            'isDataImportInProgress', 'isTransactionInProgress',
+            # PUK/PIN-Änderungsstatus
+            'hasChangedAdminPuk', 'hasChangedPuk',          # hasChangedPuk = deprecated alias
+            'hasChangedTimeAdminPuk', 'hasChangedLoggerPuk',
+            'hasChangedAdminPin', 'hasChangedTimeAdminPin', 'hasChangedLoggerPin',
+        ]:
             return bool(self.__get_info_uint32(key))
+
+        # uint64-Attribute
         elif key in ['tarExportSizeInSectors', 'tarExportSize']:
             return self.__get_info_uint64(key)
-        elif key in ['size', 'capacity', 'timeUntilNextSelfTest', 'timeUntilNextTimeSynchronization',
-                     'startedTransactions', 'maxStartedTransactions',
-                     'createdSignatures', 'maxSignatures', 'remainingSignatures', 'maxTimeSynchronizationDelay',
-                     'maxUpdateDelay', 'registeredClients', 'maxRegisteredClients', 'initializationState']:
+
+        # uint32-Attribute
+        elif key in [
+            'size', 'capacity',
+            'timeUntilNextSelfTest', 'timeUntilNextTimeSynchronization',
+            'startedTransactions', 'maxStartedTransactions',
+            'createdSignatures', 'maxSignatures', 'remainingSignatures',
+            'maxTimeSynchronizationDelay', 'maxUpdateDelay',
+            'registeredClients', 'maxRegisteredClients',
+            'initializationState',
+            # TSE v2: PUK-Blocking-Dauer (0 = nicht geblockt, >0 = Sekunden geblockt)
+            'pukBlockingDurationAdmin', 'pukBlockingDurationTimeAdmin', 'pukBlockingDurationLogger',
+            # TSE v2: aktuell eingeloggter Benutzer (WormUserId-Enum-Wert)
+            'loggedInUser',
+        ]:
             return self.__get_info_uint32(key)
-        elif key in ['customizationIdentifier', 'uniqueId']:
+
+        # uint16-Attribute
+        elif key in ['loggedInUserAutoLogOutTimeout']:
+            return self.__get_info_uint16(key)
+
+        # String-Attribute (const char*, NULL-terminiert)
+        elif key in [
+            'customizationIdentifier', 'uniqueId',   # uniqueId deprecated, nutze tseSerialNumber
+        ]:
             return self.__get_string(key)
+
+        # binary Attribute (Byte-Buffer via Längenzeiger)
         elif key in ['tsePublicKey', 'tseSerialNumber']:
             return self.__get_string64(key)
-        elif key in ['tseDescription', 'formFactor',]:
+
+        # char*-Attribute (NULL-terminierter String, kein Längenzeiger)
+        elif key in [
+            'tseCertificationId',   # SDK v6: ersetzt tseDescription
+            'tseDescription',       # deprecated alias für tseCertificationId
+            'formFactor',
+        ]:
             return self.__get_chars(key)
+
+        # Versions-Tupel (major, minor, patch)
         elif key in ['softwareVersion', 'hardwareVersion']:
             return self.__get_version(key)
-        elif key in ['certificateExpirationDate',]:
+
+        # Datum (Unix-Timestamp → datetime)
+        elif key in ['certificateExpirationDate']:
             return self.__get_date(key)
+
         else:
             raise AttributeError('unimplemented: %s' % key)
 
+    # ── private Getter ──────────────────────────────────────────────────────
 
     def __get_info_uint64(self, key):
-        getattr(self.wormlib, 'worm_info_'+key).restype = c_uint64
-        getattr(self.wormlib, 'worm_info_'+key).argtypes = (WormInfo,)
-        ret = getattr(self.wormlib, 'worm_info_'+key)(self.info)
-        return ret
+        fn = getattr(self.wormlib, 'worm_info_' + key)
+        fn.restype = c_uint64
+        fn.argtypes = (WormInfo,)
+        return fn(self.info)
 
-        
     def __get_info_uint32(self, key):
-        getattr(self.wormlib, 'worm_info_'+key).restype = c_uint32
-        getattr(self.wormlib, 'worm_info_'+key).argtypes = (WormInfo,)
-        ret = getattr(self.wormlib, 'worm_info_'+key)(self.info)
-        return ret
+        fn = getattr(self.wormlib, 'worm_info_' + key)
+        fn.restype = c_uint32
+        fn.argtypes = (WormInfo,)
+        return fn(self.info)
 
+    def __get_info_uint16(self, key):
+        fn = getattr(self.wormlib, 'worm_info_' + key)
+        fn.restype = c_uint16
+        fn.argtypes = (WormInfo,)
+        return fn(self.info)
 
     def __get_chars(self, key):
-        getattr(self.wormlib, 'worm_info_'+key).restype = c_char_p
-        getattr(self.wormlib, 'worm_info_'+key).argtypes = (WormInfo,)
-        ret = getattr(self.wormlib, 'worm_info_'+key)(self.info)
-        return ret.decode('latin1')
-
+        fn = getattr(self.wormlib, 'worm_info_' + key)
+        fn.restype = c_char_p
+        fn.argtypes = (WormInfo,)
+        ret = fn(self.info)
+        return ret.decode('latin1') if ret else ''
 
     def __get_string(self, key):
         s = c_char_p()
         sLength = c_uint()
-        getattr(self.wormlib, 'worm_info_'+key).argtypes = (WormInfo, POINTER(c_char_p), POINTER(c_uint))
-        getattr(self.wormlib, 'worm_info_'+key)(self.info, byref(s), byref(sLength))
+        fn = getattr(self.wormlib, 'worm_info_' + key)
+        fn.argtypes = (WormInfo, POINTER(c_char_p), POINTER(c_uint))
+        fn(self.info, byref(s), byref(sLength))
         s = cast(s, POINTER(c_char))
-        ret = string_at(s, size=sLength.value)
-        return ret
-
+        return string_at(s, size=sLength.value)
 
     def __get_string64(self, key):
         s = c_char_p()
         sLength = c_uint64()
-        getattr(self.wormlib, 'worm_info_'+key).argtypes = (WormInfo, POINTER(c_char_p), POINTER(c_uint64))
-        getattr(self.wormlib, 'worm_info_'+key)(self.info, byref(s), byref(sLength))
+        fn = getattr(self.wormlib, 'worm_info_' + key)
+        fn.argtypes = (WormInfo, POINTER(c_char_p), POINTER(c_uint64))
+        fn(self.info, byref(s), byref(sLength))
         s = cast(s, POINTER(c_char))
-        ret = string_at(s, size=sLength.value)
-        return ret
-        
+        return string_at(s, size=sLength.value)
+
     def __get_version(self, key):
-        getattr(self.wormlib, 'worm_info_'+key).restype = c_uint32
-        getattr(self.wormlib, 'worm_info_'+key).argtypes = (WormInfo,)
-        ret = getattr(self.wormlib, 'worm_info_'+key)(self.info)
-        major = (ret & 0xffff0000) >> 16 
-        minor = (ret & 0x0000ff00) >> 8 
-        patch = (ret & 0x000000ff) 
+        fn = getattr(self.wormlib, 'worm_info_' + key)
+        fn.restype = c_uint32
+        fn.argtypes = (WormInfo,)
+        ret = fn(self.info)
+        major = (ret & 0xFFFF0000) >> 16
+        minor = (ret & 0x0000FF00) >> 8
+        patch = (ret & 0x000000FF)
         return (major, minor, patch)
 
-
     def __get_date(self, key):
-        getattr(self.wormlib, 'worm_info_'+key).restype = c_uint64
-        getattr(self.wormlib, 'worm_info_'+key).argtypes = (WormInfo,)
-        ret = getattr(self.wormlib, 'worm_info_'+key)(self.info)
+        fn = getattr(self.wormlib, 'worm_info_' + key)
+        fn.restype = c_uint64
+        fn.argtypes = (WormInfo,)
+        ret = fn(self.info)
         return datetime.datetime.fromtimestamp(ret)
-
-
